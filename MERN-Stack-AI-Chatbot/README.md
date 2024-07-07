fa319e2e4c8e436320a85ad68d954a96b3f64d83
yadavamit1173 <103729654+yadavamit1173@users.noreply.github.com>
2024-07-08 03:00:00 +0530

    Add files via upload

---
+++ MERN-Stack-AI-Chatbot/README.md
@@ -0,0 +1,2 @@
+![image](https://github.com/JATHISWAR/MERN-Stack-AI-Chatbot/assets/43616428/bb49f33f-3cd9-467e-9450-5802940b248f)
+![image](https://github.com/JATHISWAR/MERN-Stack-AI-Chatbot/assets/43616428/09bf1744-f558-4aa2-948e-6db844c99b34)
---
+++ MERN-Stack-AI-Chatbot/frontend/src/components/footer/Footer.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div
+        style={{
+          width: "100%",
+          minHeight: "20vh",
+          maxHeight: "30vh",
+          marginTop: 60,
+        }}
+      >
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
